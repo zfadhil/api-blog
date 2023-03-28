e5acda810099d60994e5560a9e56a5d1decb0f98
FadhilYTProgrammer <fadhil18022007@gmail.com>
2023-03-28 09:06:13 +0700

    create comment resource

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 
@@ -19,5 +20,8 @@ class CommentController extends Controller
         $comment = Comment::create($request->all());
 
         return response()->json($comment);
+        // return response()->json($comment);
+
+        return new CommentResource($comment->loadMissing(['commentator:id,username']));
     }
 }
